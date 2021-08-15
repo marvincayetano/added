ae84588fef31eac6724bc3037b228ae7563d3ea8
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-08-15 10:06:33 -0400

    🔥 Chore: expo asyncstorage

--- components/Foods/FoodAdd.tsx
+++ components/Foods/FoodAdd.tsx
@@ -21,23 +21,23 @@ interface FormData {
 
 const _storeData = async () => {
   try {
-    await AsyncStorage.setItem("@MySuperStore:key", "I like to save it.");
+    await AsyncStorage.setItem("@Added", "I like to save it.");
   } catch (error) {
     // Error saving data
   }
 };
 
-const _retrieveData = async () => {
-  try {
-    const value = await AsyncStorage.getItem("TASKS");
-    if (value !== null) {
-      // We have data!!
-      console.log(value);
-    }
-  } catch (error) {
-    // Error retrieving data
-  }
-};
+// const _retrieveData = async () => {
+//   try {
+//     const value = await AsyncStorage.getItem("TASKS");
+//     if (value !== null) {
+//       // We have data!!
+//       console.log(value);
+//     }
+//   } catch (error) {
+//     // Error retrieving data
+//   }
+// };
 
 export function FoodAdd({}: FoodAddProps) {
   const {
@@ -45,6 +45,7 @@ export function FoodAdd({}: FoodAddProps) {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+
   const onSubmit = handleSubmit((data: any) => {
     // Map remove empty
     Object.keys(data).forEach((key: any) => {
