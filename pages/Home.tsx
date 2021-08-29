9153a1e4d4b3fc391de75ae9fd2b98387aa48518
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-08-29 18:07:22 -0400

    🔥 Chore: Added foods

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -9,7 +9,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 interface HomeProps {}
 
 export function Home({}: HomeProps) {
-  const [foods, setFoods] = useState<FormData[] | undefined>();
+  const [addedFoods, setAddedFoods] = useState<any | undefined>([]);
   const [availFoods, setAvailFoods] = useState<FormData[] | undefined>([]);
   useEffect(() => {
     AsyncStorage.getItem("@foods").then((value) => {
@@ -25,11 +25,41 @@ export function Home({}: HomeProps) {
         // Error retrieving data
       }
     });
+
+    AsyncStorage.getItem("@added").then((value) => {
+      try {
+        if (value !== null) {
+          // We have data!!
+          const jsonGetValue = JSON.parse(value);
+          setAddedFoods(jsonGetValue);
+          console.log(jsonGetValue);
+        }
+      } catch (error) {
+        console.log(error);
+        // Error retrieving data
+      }
+    });
   }, []);
 
-  async function AddNewFood(food: FormData, qty: number) {
-    console.log("FOOD", food);
-    console.log("QUANTITY", qty);
+  function AddNewFood(food: FormData, qty: number) {
+    try {
+      AsyncStorage.getItem("@added").then((value: string | null) => {
+        if (value && value.length) {
+          const jsonGetValue = JSON.parse(value);
+          // Check if the name is already in the storage
+
+          AsyncStorage.setItem(
+            "@added",
+            JSON.stringify([...jsonGetValue, { food, qty }])
+          );
+          return { success: "Successfully added new food!" };
+        } else {
+          AsyncStorage.setItem("@added", JSON.stringify([{ food, qty }]));
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function DeleteFood(index: number) {}
@@ -38,7 +68,7 @@ export function Home({}: HomeProps) {
     <View style={{ flex: 1 }}>
       <Info />
       <ScrollView>
-        <Macros foods={foods} />
+        <Macros foods={addedFoods} />
       </ScrollView>
       <View style={{ position: "absolute", right: 50, bottom: 100 }}>
         <ModalAdd foods={availFoods} fnAddFood={AddNewFood} />
