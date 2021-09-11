c8aa4ab4097d1eb98b9314271b868346bedd80f1
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-09-11 19:55:53 -0400

    🔥 Chore: Maxvalues asyncstorage

--- components/Info/Info.tsx
+++ components/Info/Info.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { TextInfo } from "./TextInfo";
 import { Bar } from "react-native-progress";
 import { TotalMacro } from "../../pages/Home";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface InfoProps {
   totalMacro: TotalMacro;
 }
 
 export function Info({ totalMacro }: InfoProps) {
-  console.log("WHAT", totalMacro);
+  console.log("INFO WHAT", totalMacro);
+  const [maxValues, setMaxValues] = useState<TotalMacro>({
+    calories: 0,
+    protein: 0,
+    fiber: 0,
+    carbs: 0,
+  });
+  useEffect(() => {
+    // Set the total max values here from the asyncstorage
+    try {
+      AsyncStorage.getItem("@maxvalues").then((value: string | null) => {
+        if (value) {
+          const jsonGetValue = JSON.parse(value);
+          // Check if the name is already in the storage
+          if (jsonGetValue) {
+            setMaxValues(jsonGetValue);
+          }
+
+          //   AsyncStorage.setItem(
+          //     "@added",
+          //     JSON.stringify([...jsonGetValue, { food, qty, isPerPiece }])
+          //   );
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <View>
@@ -21,17 +49,33 @@ export function Info({ totalMacro }: InfoProps) {
         </View>
       </View>
       <View style={styles.containerInfo}>
-        <TextInfo name="Calories" value={totalMacro.calories} maxValue={1000} />
-        <Bar style={styles.bar} progress={0.3} width={300} />
+        <TextInfo
+          name="Calories"
+          value={totalMacro.calories}
+          maxValue={maxValues.calories}
+        />
+        <Bar style={styles.bar} progress={0.3} width={350} />
 
-        <TextInfo name="Protein" value={totalMacro.protein} maxValue={1000} />
-        <Bar style={styles.bar} color="#f29441" progress={0.3} width={300} />
+        <TextInfo
+          name="Protein"
+          value={totalMacro.protein}
+          maxValue={maxValues.protein}
+        />
+        <Bar style={styles.bar} color="#d54d2f" progress={0.3} width={350} />
 
-        <TextInfo name="Carb" value={totalMacro.carbs} maxValue={1000} />
-        <Bar style={styles.bar} color="#f29441" progress={0.3} width={300} />
+        <TextInfo
+          name="Carb"
+          value={totalMacro.carbs}
+          maxValue={maxValues.carbs}
+        />
+        <Bar style={styles.bar} color="#0e7930" progress={0.3} width={350} />
 
-        <TextInfo name="Fiber" value={totalMacro.fiber} maxValue={1000} />
-        <Bar style={styles.bar} color="#f29441" progress={0.3} width={300} />
+        <TextInfo
+          name="Fiber"
+          value={totalMacro.fiber}
+          maxValue={maxValues.fiber}
+        />
+        <Bar style={styles.bar} color="#f29441" progress={0.3} width={350} />
       </View>
     </View>
   );
