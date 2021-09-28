9cf740caebcad655216edc2ee52849f442272c9f
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-09-27 21:08:27 -0400

    🔥 Chore: AsyncStorage utils

--- utils/AsyncStorage.tsx
+++ utils/AsyncStorage.tsx
@@ -1,3 +1,35 @@
-export function AsyncStorageGet() {}
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export function AsyncStorageSet() {}
+export function AsyncStorageGet() {
+  try {
+    AsyncStorage.getItem("@added").then((value: string | null) => {
+      if (value && value.length) {
+        const jsonGetValue = JSON.parse(value);
+        // Check if the name is already in the storage
+        return jsonGetValue;
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function AsyncStorageSet(storageName: string, setValue: any) {
+  try {
+    AsyncStorage.getItem(`@${storageName}`).then((value: string | null) => {
+      if (value && value.length) {
+        const jsonGetValue = JSON.parse(value);
+        // Check if the name is already in the storage
+
+        AsyncStorage.setItem(
+          `@${storageName}`,
+          JSON.stringify([...jsonGetValue, setValue])
+        );
+      } else {
+        AsyncStorage.setItem(`@${storageName}`, JSON.stringify([setValue]));
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
